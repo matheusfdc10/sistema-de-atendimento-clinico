2c51ed47492df9292d5d877f41604224d9270f2f
matheusfdc10 <64614285+matheusfdc10@users.noreply.github.com>
2023-07-06 18:48:03 -0300

    up

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,37 +1,46 @@
+import moment from 'moment-timezone';
+
+moment.tz.setDefault('America/Sao_Paulo');
+
+
 export const formatDateTime = (dateTime: Date | null) => {
     if(!dateTime) return ''
 
     // Creating a Date object with the provided date string
-    // var date = new Date(dateTime);
+    var date = moment(dateTime).format('DD/MM/YYYY - HH:mm');
+    return date
   
     // Extracting the relevant parts of the date
-    var year = dateTime.getFullYear();
-    var month = ("0" + (dateTime.getMonth() + 1)).slice(-2); // Adds a leading zero if necessary
-    var day = ("0" + dateTime.getDay()).slice(-2); // Adds a leading zero if necessary
+    // var year = dateTime.getFullYear();
+    // var month = ("0" + (dateTime.getMonth() + 1)).slice(-2); // Adds a leading zero if necessary
+    // var day = ("0" + dateTime.getDay()).slice(-2); // Adds a leading zero if necessary
 
-    var hours = ("0" + dateTime.getHours()).slice(-2); // Adds a leading zero if necessary
-    var minutes = ("0" + dateTime.getMinutes()).slice(-2); // Adds a leading zero if necessary
-    var seconds = ("0" + dateTime.getSeconds()).slice(-2); // Adds a leading zero if necessary
+    // var hours = ("0" + dateTime.getHours()).slice(-2); // Adds a leading zero if necessary
+    // var minutes = ("0" + dateTime.getMinutes()).slice(-2); // Adds a leading zero if necessary
+    // var seconds = ("0" + dateTime.getSeconds()).slice(-2); // Adds a leading zero if necessary
 
     // Constructing the date string
-    var formattedDate = day + "/" + month + "/" + year + " - " + hours + ":" + minutes;
+    // var formattedDate = day + "/" + month + "/" + year + " - " + hours + ":" + minutes;
 
     // Returning the formatted date
-    return formattedDate;
+    // return formattedDate;
 }
 
 export const formatTime = (dateTime: Date | null) => {
     if(!dateTime) return ''
 
+    var date = moment(dateTime).format('HH:mm');
+    return date
+
     // Creating a Date object with the provided date string
     // var date = new Date(dateTime);
 
-    var hours = ("0" + dateTime.getHours()).slice(-2); // Adds a leading zero if necessary
-    var minutes = ("0" + dateTime.getMinutes()).slice(-2); // Adds a leading zero if necessary
+    // var hours = ("0" + dateTime.getHours()).slice(-2); // Adds a leading zero if necessary
+    // var minutes = ("0" + dateTime.getMinutes()).slice(-2); // Adds a leading zero if necessary
 
-    // Constructing the date string
-    var formattedDate = hours + ":" + minutes;
+    // // Constructing the date string
+    // var formattedDate = hours + ":" + minutes;
 
-    // Returning the formatted date
-    return formattedDate;
+    // // Returning the formatted date
+    // return formattedDate;
 }
